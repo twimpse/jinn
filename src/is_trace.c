aceed092c2836e9b076a68fd70f0f723ebe10ec5
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-08 02:16:29 +0200

    Added #define DEV_RISKY_MODE to is_trace.c

--- src/is_trace.c
+++ src/is_trace.c
@@ -3,11 +3,18 @@
 #include <sys/prctl.h>
 
 int is_debugged(void) {
+#ifdef DEV_RISKY_MODE
+  return 0;
+#else
   return ptrace(PTRACE_TRACEME, 0, 1, 0) == -1;
+#endif
 }
 
-void prevent_trace(void) {
+int prevent_trace(void) {
+#ifndef DEV_RISKY_MODE
 // Prevent ptrace attachment (Linux)
   prctl(PR_SET_DUMPABLE, 0);  // Disable core dumps, often blocks debugging
   prctl(PR_SET_PTRACER, 0);   // Only allow PID 0 (no one) to attach
+#endif
+return 0;
 }
