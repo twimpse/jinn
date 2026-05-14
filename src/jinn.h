e20d90113673fb4fa278a83f2e15ee36425fa558
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-14 07:37:28 +0200

    edited jinn.h and added base

--- src/jinn.h
+++ src/jinn.h
@@ -1,1 +1,13 @@
-#DEFINE SEED 
+
+// sha512(md5sum(md5sum("zero"))
+#DEFINE SEED "a76c1de5fab3e09bc8f50225018da99a17c91addc1851ff7b201d41541a36cabe2c8b35f0477511368ab97e2da7fcb7a079a17379efb2a29c410454bb2fd9083"
+#DEFINE CODE_KEY "xAA\xBB\xCC\xDD\xEE\xFF"
+#DEFINE CODE_BUFFER_BOOKEND "\x00000000,\x00000000,\x00000000\x00000000,\x00000000,\x00000000,\x00000000\x00000000"
+#DEFINE VERSION 0.0.1-pre-beta-dev
+#DEFINE LISTEN_PORT 17283
+#DEFINE MAGIC_8_BALL_MODE "genie"
+#DEFINE TRANSPARENT_PROXY "dp"
+#DEFINE ADMIN_USERNAME "controller"
+#DEFINE ADMIN_PASSWORD "encryptedstring" 
+// how large do we make our custom dsta section in the .rodata and .data parts. keep about 1024 bits as low entropy. 
+#DEFINE SECTION_BUFFER_SIZE 4096
