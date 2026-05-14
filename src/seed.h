ae9983e1dbe701c9dc70b706d96b0bd476a94926
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-14 08:05:06 +0200

    Added fixed seed.h

---
+++ src/seed.h
@@ -0,0 +1,6 @@
+#ifndef SEED_H
+#define SEED_H
+
+#define SEED "a76c1de5fab3e09bc8f50225018da99a17c91addc1851ff7b201d41541a36cabe2c8b35f0477511368ab97e2da7fcb7a079a17379efb2a29c410454bb2fd9083"
+
+#endif /* SEED_H */
