26b226bc8cb3bf1992cc514eee7b0e469ba6a11c
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-15 15:58:17 +0200

    general commit

--- src/jinn.h
+++ src/jinn.h
@@ -10,3 +10,14 @@
 #DEFINE ADMIN_PASSWORD "encryptedstring" 
 // how large do we make our custom dsta section in the .rodata and .data parts. keep about 1024 bits as low entropy. 
 #DEFINE SECTION_BUFFER_SIZE 4096
+
+char *log_path = NULL;
+int debugged = 0;
+const char build_key[] = SEED;
+const char admin_user[] = ADMIN_USERNAME;
+char admin_pass[] = ADMIN_PASSWORD;
+char *own_name = NULL;
+char *own_key = NULL;
+
+bool is_traced = false;
+bool isset_rand = false;
