66fc0106c0089392f0a52d5b2d81933c16e45c7a
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-18 10:30:59 +0200

    Added jinn_base.c

---
+++ src/jinn_base.c
@@ -0,0 +1,28 @@
+
+int print_banner() {
+
+}
+
+int print_name() {
+
+};
+
+int print_version() {
+
+};
+
+int print_line() {
+
+};
+
+int print_line_long() {
+
+};
+
+int print_line_short() {
+
+};
+
+int print_break() {
+
+}
