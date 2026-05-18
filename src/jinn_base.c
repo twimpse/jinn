610a013a23e667e3be8feae165d1f53093bbc01e
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-18 17:25:21 +0200

    added to base.c

--- src/jinn_base.c
+++ src/jinn_base.c
@@ -1,6 +1,9 @@
+#include "system_messages.h"
+
 
 int print_banner() {
-
+ random = rand(srand()) %10;
+ printf(banner[random]);
 }
 
 int print_name() {
@@ -12,6 +15,29 @@ int print_version() {
 };
 
 int print_line() {
+  int rand = rand(srand()) %4;
+  char response
+  switch(rand){
+    case 0:
+      int loops = 8;
+      while (loops > 0) {
+        sprintf(print_line_short());
+        loops--;
+      }
+      return response;
+    break;
+    case 1:
+     for (int i=0; i<4;i++) {
+       sprintf(print_line_long());
+     }
+    break;
+    case 2:
+     sprintf(print_line_long(), print_line_short(), print_line_long(), print_line_short(), print_line_long(), print_line_short());
+    break;
+    case 4:
+     sprintf(print_line_short(), print_line_long(), print_line_short(), print_line_long(), print_line_short(),print_line_long());
+    break;
+  }
 
 };
 
@@ -26,3 +52,7 @@ int print_line_short() {
 int print_break() {
 
 }
+
+int print_usage() {
+
+};
