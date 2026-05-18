18d726d15770b360d09b71e12c6527a0ed66ee18
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-18 10:56:17 +0200

    added struct for mud

--- src/jinn_mud.c
+++ src/jinn_mud.c
@@ -1,4 +1,6 @@
 // MuD game goes here
+#DEFINE ROOM_DESCRIPTION_MAX_LEN 128
+
 
 int mud_health
 int mud_gold
@@ -7,3 +9,17 @@ int mud_progress
 int mud_level
 char *mud_hero_name[32];
 char *mud_villan_name[32];
+char *mud_dragon_name[32];
+char *nud_wizards_name[32];
+char *mud_elf_name[32];
+
+
+typedef struct Room {
+  char description[ROOM_DESCRIPTION_MAX_LEN]
+  struct Room *north;
+  struct Room *south;
+  struct Room *east;
+  struct Room *west;
+  int has_monster;
+  int has_chest;
+  int has_loot;
